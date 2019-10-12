e19d9ce9d7b936f64cf503391454245f3e5a0f85
Luke Edwards <luke.edwards05@gmail.com>
2019-10-12 15:24:41 -0700

    feat: initial pattern

--- local.d.ts
+++ local.d.ts
@@ -1,1 +1,1 @@
-//
+export default function (str: stirng) => boolean;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,111 @@
 const test = require('tape');
-const lib = require('../dist/local');
+const check = require('../dist/local');
+
+test.Test.prototype.local = function (val, bool) {
+	const out = check(val);
+	this.is(out, bool, `(${bool}) "${val}"`);
+};
 
 test('exports', t => {
-	t.is(typeof lib, 'function', 'exports a function');
+	t.is(typeof check, 'function', 'exports a function');
+	t.end();
+});
+
+test('unspecified', t => {
+	t.local('::', true);
+	t.local(':::', false);
+	t.end();
+});
+
+test('ipv6 localhost', t => {
+	t.local('::1', true);
+	t.local('::2', false);
+	t.end();
+});
+
+test('localhost special', t => {
+	t.local('localhost', true);
+	t.local('.localhost', true);
+	t.local('foo.localhost', true);
+	t.local('foo.bar.localhost', true);
+
+	t.local('localhost1', false);
+	t.local('.localhost1', false);
+	t.local('123localhost', false);
+
+	t.end();
+});
+
+test('127.0.0.0/8', t => {
+	t.local('127.0.0.0', true);
+	t.local('127.10.10.10', true);
+	t.local('127.255.255.255', true);
+	t.local('127.0.0.1', true);
+
+	t.local('127.300.300.300', false); // TODO
+	t.local('128.0.0.1', false);
+	t.local('127...1', false);
+	t.local('127.1', false);
+
+	t.end();
+});
+
+test('10.0.0.0/8', t => {
+	t.local('10.0.0.0', true);
+	t.local('10.0.0.10', true);
+	t.local('10.0.0.100', true);
+	t.local('10.10.10.10', true);
+	t.local('10.254.254.10', true);
+	t.local('10.255.255.255', true);
+
+	t.local('11.0.0.0', false);
+	t.local('100.0.0.0', false);
+	t.local('10.300.300.300', false); // TODO
+	t.local('10.2550.0.1', false);
+	t.local('10.254.0', false);
+
+	t.end();
+});
+
+test('192.168.0.0/16', t => {
+	t.local('192.168.0.0', true);
+	t.local('192.168.255.255', true);
+	t.local('192.168.0.1', true);
+
+	t.local('192.167.0.0', false);
+	t.local('192 168 255 255', false);
+	t.local('192.168.0.300', false); // TODO
+	t.local('193.168.0.1', false);
+
+	t.end();
+});
+
+test('172.16.0.0/12', t => {
+	t.local('172.16.0.0', true);
+	t.local('172.24.255.0', true);
+	t.local('172.31.255.255', true);
+	t.local('172.31.0.99', true);
+
+	t.local('172.32.0.0', false);
+	t.local('172.160.0.0', false);
+	t.local('172.16.0.256', false); // TODO
+	t.local('173.168.0.1', false);
+	t.local('172.15.0.0', false);
+
+	t.end();
+});
+
+test('169.254.0.0/16', t => {
+	t.local('169.254.1.0', true);
+	t.local('169.254.1.255', true);
+	t.local('169.254.254.255', true);
+	t.local('169.254.254.100', true);
+	t.local('169.254.254.0', true);
+
+	t.local('169.254.0.0', false);
+	t.local('169.254.255.0', false);
+	t.local('169.254.254.256', false); // TODO
+	t.local('168.254.1.0', false);
+
 	t.end();
 });
